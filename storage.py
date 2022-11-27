5a8d2ded1b02c4bf833fda8928ca27e45a693d58
arfana017 <70667415+arfana017@users.noreply.github.com>
2022-11-27 10:05:22 -0800

    Add files via upload

---
+++ storage.py
@@ -0,0 +1,11 @@
+import csv
+
+all_movies = []
+with open("final.csv", encoding="utf8") as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    all_movies = data[1:]
+
+liked_movies = []
+unliked_movies = []
+did_not_watch = []
